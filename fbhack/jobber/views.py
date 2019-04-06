44cbd2c5f8a087f478858b1d1ec4a120ecf8068c
Jakob Wolitzki <jakob.wolitzki@gmail.com>
2019-04-06 18:33:25 +0200

    new changes

--- fbhack/jobber/views.py
+++ fbhack/jobber/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from jobber.models import Job
+
+
 
 def home(request):
-    if request.method == 'POST':
-        print(request.POST['job'])
-        
     context = {}
+    if request.method == 'POST':
+        job_name = request.POST['job']
+        job_obj = Job.objects.filter(job__icontains=job_name).first()
+        danger = 'safe' if job_obj.probability < 0.5 else 'unsafe'
+        context = {
+            'job': job_obj.job,
+            'danger': danger
+        }
+        return check_job(request, context)
+    
     return render(request,'jobber/homepage.html',context)
+
+
+def check_job(request, context):
+    return render(request,'jobber/check.html',context)
+
 
 # Create your views here.
