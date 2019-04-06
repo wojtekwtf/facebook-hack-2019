e701bddbd55903f245db3d146aeee10a3948707b
wkulikowski <wojtask98@gmail.com>
2019-04-06 15:06:20 +0200

    personalities weights

---
+++ personality_to_job.py
@@ -0,0 +1,36 @@
+"""
+Therapist:
+    Extroversion 0.5
+    Neuroticism 0.5
+    Agreeableness 1
+    Conscientiousness 1
+    Openness 1
+Personal Trainers:
+    Extroversion 1
+    Neuroticism 0.5 
+    Agreeableness 0.5
+    Conscientiousness 1
+    Openness 1
+Software Developer:
+    Extroversion 0
+    Neuroticism 0.5
+    Agreeableness 0.5
+    Conscientiousness 1
+    Openness 0
+Chef:
+    Extroversion 0.5
+    Neuroticism 1
+    Agreeableness 1
+    Conscientiousness 1
+    Openness 0.5
+Sales Engineer:
+    Extroversion 1
+    Neuroticism 0.5
+    Agreeableness 0.5
+    Conscientiousness 1
+    Openness 1
+"""
+
+def personality_to_job(personality_dict):
+
+    pass
